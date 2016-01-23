056a183ee28007ae45781b7b90d94079675e5125
栋栋晓 <2243980491@qq.com>
2016-01-23 20:00:03 +0800

    Added post form to components sub directory.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,7 @@ class PagesController < ApplicationController
       redirect_to root_path, :notice=> "User not found!"
     end
     @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+  	@newPost = Post.new
   end
 
   def explore
